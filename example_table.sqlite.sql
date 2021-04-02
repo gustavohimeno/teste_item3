a9e5f3d2a7fa8dad27fe50146f3e2d5ce3dce5e6
gustavohimeno <58175587+gustavohimeno@users.noreply.github.com>
2021-04-02 17:49:24 -0300

    atualização estrutura tabela

--- example_table.sqlite.sql
+++ example_table.sqlite.sql
@@ -1,6 +1,5 @@
 -- TABLE
 CREATE TABLE Notes ( Grade INTEGER, Min_Value Integer, Max_Value INTEGER );
-CREATE TABLE sqlite_sequence(name,seq);
 CREATE TABLE Students ( ID INTEGER PRIMARY KEY AUTOINCREMENT, Name STRING, Value INTEGER );
  
 -- INDEX
